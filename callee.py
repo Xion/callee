c7841c5bebc226bd0c873bb5a9e751e83a7d067d
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2015-08-23 13:13:54 -0700

    Simplest implementation of Any

---
+++ callee.py
@@ -0,0 +1,23 @@
+"""
+callee :: Argument matcher for unittest.mock
+"""
+__version__ = "0.0.1"
+__author__ = "Karol Kuczmarski"
+__license__ = "Simplified BSD"
+
+
+__all__ = ['Any']
+
+
+class Matcher(object):
+    """Base class for argument matchers."""
+
+    def __eq__(self, other):
+        raise NotImplementedError("matching not implemented")
+
+
+class Any(object):
+    """Matches any object."""
+
+    def __eq__(self, other):
+        return True
