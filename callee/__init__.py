93c28d8814b0d6039a5454dd262e71cf941cbed9
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2015-09-16 22:08:33 -0700

    Export the previously added matchers

--- callee/__init__.py
+++ callee/__init__.py
@@ -8,16 +8,19 @@ __license__ = "Simplified BSD"
 
 
 from callee.base import And, Or, Not
-from callee.general import Any, ArgThat, IsA, InstanceOf, Matching
+from callee.general import \
+    Any, ArgThat, IsA, Inherits, InstanceOf, Matching, SubclassOf
 from callee.strings import Bytes, String, Unicode
 
 
 __all__ = [
     'Not', 'And', 'Or',
-    'Any', 'Matching', 'ArgThat', 'InstanceOf', 'IsA',
+    'Any',
+    'Matching', 'ArgThat', 'InstanceOf', 'IsA', 'SubclassOf', 'Inherits',
     'String', 'Unicode', 'Bytes',
 ]
 
 
+# TODO(xion): operator-based matchers (GreaterThan, ShorterThan, etc.)
 # TODO(xion): collection matchers (lists, sequences, dicts, ...)
 # TODO(xion): matchers for positional & keyword arguments
