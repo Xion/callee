9a43872a106c9113ed2d4ede38c4bc2e8b8a5569
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2016-03-16 19:32:16 -0700

    Tests for Attrs.__repr__

--- tests/test_general.py
+++ tests/test_general.py
@@ -263,16 +263,16 @@ class SubclassOf(MatcherTestCase):
 class Attrs(MatcherTestCase):
     VALUE = 42
 
-    def test_invalid(self):
+    def test_match__invalid(self):
         with self.assertRaises(TypeError):
             self.assert_match('unused')
 
-    def test_names_only__one(self):
+    def test_match__names_only__one(self):
         foo = self.Object(foo=self.VALUE)
         self.assert_match(foo, 'foo')
         self.assert_no_match(foo, 'bar')
 
-    def test_names_only__several(self):
+    def test_match__names_only__several(self):
         foo_bar = self.Object(foo=self.VALUE, bar=self.VALUE * 2)
 
         self.assert_match(foo_bar, 'foo')
@@ -282,7 +282,7 @@ class Attrs(MatcherTestCase):
         self.assert_no_match(foo_bar, 'baz')
         self.assert_no_match(foo_bar, 'foo', 'baz')
 
-    def test_values_only__one(self):
+    def test_match__values_only__one(self):
         foo = self.Object(foo=self.VALUE)
 
         self.assert_match(foo, foo=self.VALUE)
@@ -290,7 +290,7 @@ class Attrs(MatcherTestCase):
         self.assert_no_match(foo, foo=self.VALUE + 1)
         self.assert_no_match(foo, bar=self.VALUE)
 
-    def test_values_only__several(self):
+    def test_match__values_only__several(self):
         foo_bar = self.Object(foo=self.VALUE, bar=self.VALUE * 2)
 
         self.assert_match(foo_bar, foo=self.VALUE)
@@ -300,7 +300,7 @@ class Attrs(MatcherTestCase):
         self.assert_no_match(foo_bar, foo=self.VALUE + 1, bar=self.VALUE)
         self.assert_no_match(foo_bar, foo=self.VALUE, baz=self.VALUE * 2)
 
-    def test_both__one_each(self):
+    def test_match__both__one_each(self):
         foo_bar = self.Object(foo=self.VALUE, bar=self.VALUE * 2)
 
         self.assert_match(foo_bar, 'foo', bar=self.VALUE * 2)
@@ -309,7 +309,7 @@ class Attrs(MatcherTestCase):
         self.assert_no_match(foo_bar, 'foo', baz=self.VALUE * 2)
         self.assert_no_match(foo_bar, 'baz', foo=self.VALUE)
 
-    def test_both__mix(self):
+    def test_match__both__mix(self):
         attrs = dict(foo=self.VALUE,
                      bar=self.VALUE * 2,
                      baz=self.VALUE * 2 + 1)
@@ -323,7 +323,54 @@ class Attrs(MatcherTestCase):
         self.assert_no_match(foo_bar_baz, 'qux', **attrs)
         self.assert_no_match(foo_bar_baz, 'foo', 'bar', baz='wrong value')
 
-    # TODO(xion): test the __repr__ method
+    def test_repr__names_only__one(self):
+        attrs = __unit__.Attrs('foo')
+
+        r = "%r" % attrs
+        self.assertIn("foo", r)
+        self.assertNotIn("=", r)  # because no value was given
+
+    def test_repr__names_only__several(self):
+        attrs = __unit__.Attrs('foo', 'bar')
+
+        r = "%r" % attrs
+        self.assertIn("foo", r)
+        self.assertIn("bar", r)
+        self.assertNotIn("=", r)
+
+    def test_repr__values_only__one(self):
+        attrs = __unit__.Attrs(foo=self.VALUE)
+
+        r = "%r" % attrs
+        self.assertIn("foo=", r)
+        self.assertIn("%r" % self.VALUE, r)
+
+    def test_repr__values_only__several(self):
+        attrs = __unit__.Attrs(foo=self.VALUE, bar=self.VALUE * 2)
+
+        r = "%r" % attrs
+        self.assertIn("foo=", r)
+        self.assertIn("bar=", r)
+        self.assertIn("%r" % self.VALUE, r)
+
+    def test_repr__both__one_each(self):
+        attrs = __unit__.Attrs('bar', foo=self.VALUE)
+
+        r = "%r" % attrs
+        self.assertIn("bar", r)
+        self.assertNotIn("bar=", r)  # because this one doesn't have a value
+        self.assertIn("foo=", r)
+        self.assertIn("%r" % self.VALUE, r)
+
+    def test_repr__both__mix(self):
+        attrs = __unit__.Attrs('bar', 'baz', foo=self.VALUE)
+
+        r = "%r" % attrs
+        self.assertIn("bar", r)
+        self.assertNotIn("bar=", r)
+        self.assertIn("baz", r)
+        self.assertNotIn("baz=", r)
+        self.assertIn("%r" % self.VALUE, r)
 
     # Utility code
 
