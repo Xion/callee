d0e19ea2afdbaacebb2ef7ad6d5ad214eb00c9b4
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2016-05-16 22:06:52 -0700

    Ask for confirmation before uploading to PyPI

--- tasks.py
+++ tasks.py
@@ -1,6 +1,7 @@
 """
 Automation tasks, aided by the Invoke package.
 """
+import logging
 import os
 import webbrowser
 import sys
@@ -29,7 +30,26 @@ def docs(output='html', rebuild=False, show=True):
         webbrowser.open_new_tab(path)
 
 
-@task
-def upload():
+@task(help={'yes': "Whether to actually perform the upload. "
+                   "By default, a confirmation is necessary."})
+def upload(yes=False):
     """Upload the package to PyPI."""
-    run('python setup.py sdist upload')
+    # check the packages version
+    # TODO: add a 'release' to automatically bless a version as release one
+    import callee
+    if callee.__version__.endswith('-dev'):
+        logging.error("Can't upload a development version to PyPI!")
+        return -1
+
+    # run the upload if it has been confirmed by the user
+    if not yes:
+        answer = raw_input("Do you really want to upload to PyPI [y/N]? ")
+        yes = answer.lower() == 'y'
+    if yes:
+        logging.debug("Running PyPI upload...")
+        if run('python setup.py sdist upload'):
+            logging.info("PyPI upload completed successfully.")
+        else:
+            logging.error("Failed to upload to PyPI!")
+    else:
+        logging.warning("Aborted -- not uploading to PyPI.")
