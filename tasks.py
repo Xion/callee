ffde1906369585f4f1d49eb3216dfd622f997e44
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2016-05-19 22:08:16 -0700

    Further improve the upload task

--- tasks.py
+++ tasks.py
@@ -13,9 +13,11 @@ DOCS_DIR = 'docs'
 DOCS_OUTPUT_DIR = os.path.join(DOCS_DIR, '_build')
 
 
-@task(help={'output': "Documentation output format to produce",
-            'rebuild': "Whether to rebuild the documentation from scratch",
-            'show': "Whether to show the docs in the browser (default: yes)"})
+@task(help={
+    'output': "Documentation output format to produce",
+    'rebuild': "Whether to rebuild the documentation from scratch",
+    'show': "Whether to show the docs in the browser (default: yes)",
+})
 def docs(output='html', rebuild=False, show=True):
     """Build the docs and show them in default web browser."""
     build_cmd = 'sphinx-build -b {output} {all} docs docs/_build'.format(
@@ -30,26 +32,45 @@ def docs(output='html', rebuild=False, show=True):
         webbrowser.open_new_tab(path)
 
 
-@task(help={'yes': "Whether to actually perform the upload. "
-                   "By default, a confirmation is necessary."})
+@task(help={
+    'yes': "Whether to actually perform the upload. "
+           "By default, a confirmation is necessary.",
+})
 def upload(yes=False):
     """Upload the package to PyPI."""
+    import callee
+    version = callee.__version__
+
     # check the packages version
     # TODO: add a 'release' to automatically bless a version as release one
-    import callee
-    if callee.__version__.endswith('-dev'):
-        logging.error("Can't upload a development version to PyPI!")
-        return -1
+    if version.endswith('-dev'):
+        fatal("Can't upload a development version (%s) to PyPI!", version)
 
     # run the upload if it has been confirmed by the user
     if not yes:
         answer = raw_input("Do you really want to upload to PyPI [y/N]? ")
         yes = answer.lower() == 'y'
     if yes:
-        logging.debug("Running PyPI upload...")
+        logging.debug("Uploading version %s to PyPI...", version)
         if run('python setup.py sdist upload'):
             logging.info("PyPI upload completed successfully.")
         else:
-            logging.error("Failed to upload to PyPI!")
+            fatal("Failed to upload version %s to PyPI!", version)
     else:
         logging.warning("Aborted -- not uploading to PyPI.")
+        return -2
+
+    # add a Git tag and push
+    if not run('git tag %s' % version):
+        fatal("Failed to add a Git tag for uploaded version %s", version)
+    if not run('git push && git push --tags'):
+        fatal("Failed to push the release upstream.")
+
+
+# Utility functions
+
+def fatal(*args, **kwargs):
+    """Log an error message and exit."""
+    exitcode = kwargs.pop('exitcode', -1)
+    logging.error(*args, **kwargs)
+    sys.exit(exitcode)
